b805e65e7f36a77aff73407991ed387dbf83870d
Abbas <abbas.s786@gmail.com>
2013-09-06 08:02:19 +0530

    Created The Android Project

---
+++ EasyContactFormAndroid/src/com/easycontactformandroid/EasyContactFormActivity.java
@@ -0,0 +1,23 @@
+package com.easycontactformandroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EasyContactFormActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_contact_form);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.easy_contact_form, menu);
+        return true;
+    }
+    
+}
