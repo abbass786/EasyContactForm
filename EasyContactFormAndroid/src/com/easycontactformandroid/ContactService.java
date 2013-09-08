bd8135ce9e9613717e8bef88b03bb180ef4b37b9
Abbas Suterwala <abbas.s786@gmail.com>
2013-09-08 13:31:18 +0530

    Added the Contact service

---
+++ EasyContactFormAndroid/src/com/easycontactformandroid/ContactService.java
@@ -0,0 +1,20 @@
+package com.easycontactformandroid;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class ContactService extends IntentService{
+	public ContactService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+    protected void onHandleIntent(Intent workIntent) {
+        // Gets data from the incoming Intent
+        String dataString = workIntent.getDataString();
+        //...
+        // Do work here, based on the contents of dataString
+        ...
+    }
+}
