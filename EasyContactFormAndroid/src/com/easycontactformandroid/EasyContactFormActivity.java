d91d163fb6c7d64846f380c6b791408c4b265b96
Meenal Mundle <meenal.mundle@gmail.com>
2013-09-09 12:15:42 +0530

    Made Changes to remove the menu from the activity

--- EasyContactFormAndroid/src/com/easycontactformandroid/EasyContactFormActivity.java
+++ EasyContactFormAndroid/src/com/easycontactformandroid/EasyContactFormActivity.java
@@ -16,7 +16,7 @@ public class EasyContactFormActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.easy_contact_form, menu);
+        //getMenuInflater().inflate(R.menu.easy_contact_form, menu);
         return true;
     }
     
